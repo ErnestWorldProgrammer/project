81153a7368bba86b68baa52486e74e5f80d482c9
ErnestWorldProgrammer <169020960+ErnestWorldProgrammer@users.noreply.github.com>
2024-11-18 14:02:22 +0600

    домашка5
    домашка5

--- project/project/js/lesson.js
+++ project/project/js/lesson.js
@@ -49,3 +49,39 @@ tabsParent.onclick = (event) => {
 }
 hideTabCont()
 showTabContent()
+
+let somInput = document.querySelector("#som")
+let usdInput = document.querySelector("#usd")
+let eurInput = document.querySelector("#eur");
+
+let converter = (element, targetElements) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send();
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+            targetElements.forEach(target => {
+                if (element.id === 'som') {
+                    if (target.id === 'usd') target.value = (element.value / data.usd).toFixed(2);
+                    if (target.id === 'eur') target.value = (element.value / data.eur).toFixed(2);
+                }
+                if (element.id === 'usd') {
+                    if (target.id === 'som') target.value = (element.value * data.usd).toFixed(2);
+                    if (target.id === 'eur') target.value = (element.value * data.usd / data.eur).toFixed(2);
+                }
+                if (element.id === 'eur') {
+                    if (target.id === 'som') target.value = (element.value * data.eur).toFixed(2);
+                    if (target.id === 'usd') target.value = (element.value * data.eur / data.usd).toFixed(2);
+                }
+                if (element.value === '') {
+                    target.value = '';
+                }
+            });
+        };
+    };
+};
+converter(somInput, [usdInput, eurInput]);
+converter(usdInput, [somInput, eurInput]);
+converter(eurInput, [somInput, usdInput]);
